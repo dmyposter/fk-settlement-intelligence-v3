4a5cae7d5c9d1a103273790cf20f5f26c009eba1
Ankit <ankitrajput.email@gmail.com>
2026-06-19 17:31:29 +0530

    Firebase Login Signup

---
+++ public/auth.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAWKKN4UBHpdtXn4nbE4oo77yp9aoZaPVo",
+  authDomain: "fk-settlement-intelligence.firebaseapp.com",
+  projectId: "fk-settlement-intelligence",
+  storageBucket: "fk-settlement-intelligence.firebasestorage.app",
+  messagingSenderId: "177524803194",
+  appId: "1:177524803194:web:53e710f44f3f9d12621dc3"
+};
+
+firebase.initializeApp(firebaseConfig);
